ae3067fb104d8dd7f0a7ae732ca46245b81a96ff
Scott J. Kleper <klep@venturehacks.com>
2016-12-23 11:12:34 -0800

    Initial Commit

---
+++ SKBatcher/SKBatcher.h
@@ -0,0 +1,19 @@
+//
+//  SKBatcher.h
+//  SKBatcher
+//
+//  Created by Scott J. Kleper on 12/23/16.
+//  Copyright © 2016 Boat Launch, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SKBatcher.
+FOUNDATION_EXPORT double SKBatcherVersionNumber;
+
+//! Project version string for SKBatcher.
+FOUNDATION_EXPORT const unsigned char SKBatcherVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SKBatcher/PublicHeader.h>
+
+
